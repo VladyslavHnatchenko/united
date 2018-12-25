83470f04aef3aa2e686fb3e9454805613ea81cf1
vladyslav.hnatchenko <hnatchenko.vladyslav@gmail.com>
2018-12-25 16:56:52 +0200

    12/25/18

---
+++ quality_p.py
@@ -0,0 +1,87 @@
+"""code review"""
+
+
+class Downloader:
+
+    def __init__(self, urls, timeout, max_attempts):
+        self.urls = urls
+        self.timeout = timeout
+        self.max_attempts = max_attempts
+
+# urls = []
+# for path in paths:
+#     url = 'https://'
+#     url += domain
+#     url += path
+#     urls.append(url)
+#
+# # I propose to consider the list inclusion of this kind:
+# urls = ['https://' + domain + path for path in paths]
+
+
+# import sys
+#
+#
+# class CarClass:
+#     """"""
+#
+#     def __init__(self, color, make, model, year):
+#         """Constructor"""
+#         self.color = color
+#         self.make = make
+#         self.model = model
+#         self.year = year
+#
+#         if "Windows" in platform.platform():
+#             print("You're using Windows!")
+#
+#         self.weight = self.getWeight(1, 2, 3)
+#
+#     def getWeight(this):
+#         """"""
+#         return "2000 lbs"
+
+# import platform
+#
+#
+# class CarClass:
+#     """"""
+#
+#     def __init__(self, color, make, model, year):
+#         """Constructor"""
+#         self.color = color
+#         self.make = make
+#         self.model = model
+#         self.year = year
+#
+#         if "Windows" in platform.platform():
+#             print("You're using Windows!")
+#
+#         self.weight = self.get_weight(3)
+#
+#     def get_weight(self, this):
+#         """"""
+#         return "2000 lbs"
+
+
+# import sys
+#
+#
+# class CarClass:
+#     """"""
+#
+#     def __init__(self, color, make, model, year):
+#         """Constructor"""
+#         self.color = color
+#         self.make = make
+#         self.model = model
+#         self.year = year
+#
+#         if "Windows" in platform.platform():
+#             print("You're using Windows!")
+#
+#         self.weight = self.getWeight(1, 2, 3)
+#
+#     def getWeight(this):
+#         """"""
+#         return "2000 lbs"
