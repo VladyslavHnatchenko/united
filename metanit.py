4dafb7ffe538cc639e13753832b12dd1a0a6578e
vladyslav.hnatchenko <hnatchenko.vladyslav@gmail.com>
2019-01-15 16:29:14 +0200

    1/15/19

---
+++ metanit.py
@@ -0,0 +1,85 @@
+"""modules"""
+
+
+"""scope"""
+# def say_hi():
+#     name = "Sam"
+#     surname = "Johnson"
+#     print("Hello", name, surname)
+#
+#
+# def say_bye():
+#     name = "Tom"
+#     print("Good bye", name)
+#
+#
+# say_hi()
+# say_bye()
+
+# name = "Tom"
+#
+#
+# def say_hi():
+#     print("Hello", name)
+#
+#
+# def say_bye():
+#     print("Good bye", name)
+#
+#
+# say_hi()
+# say_bye()
+
+"""function"""
+# def main():
+#     say_hello("Tom")
+#     usd_rate = 28
+#     money = 40000
+#     result = exchange(usd_rate, money)
+#     print("To be received", result, "usd")
+#
+#
+# def say_hello(name):
+#     print("Hello, ", name)
+#
+#
+# def exchange(usd_rate, money):
+#     result = round(money/usd_rate, 2)
+#     return result
+#
+#
+# main()
+
+# def exchange(usd_rate, money):
+#     result = round(money/usd_rate, 2)
+#     return result
+#
+#
+# result_1 = exchange(28, 40000)
+# print(result_1)
+#
+# result_2 = exchange(28, 80000)
+# print(result_2)
+#
+# result_3 = exchange(28, 120000)
+# print(result_3)
+
+# def sum(*params):
+#     result = 0
+#     for n in params:
+#         result += n
+#     return result
+#
+#
+# sum_of_numbers = sum(1, 2, 3, 4, 5)
+# sum_of_numbers_2 = sum(4, 22, 2222)
+#
+# print(sum_of_numbers)
+# print(sum_of_numbers_2)
+
+# def say_hello(name):
+#     print("Hello", name)
+#
+#
+# say_hello("Tom")
+# say_hello("Alice")
