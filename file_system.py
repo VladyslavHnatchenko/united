ce471ff35596f389297c0374ae508858789f0ba4
vladyslav.hnatchenko <hnatchenko.vladyslav@gmail.com>
2019-01-21 08:41:10 +0200

    1/21/19

--- file_system.py
+++ file_system.py
@@ -1,23 +1,78 @@
+"""os"""
+import os
+
+
+os.rmdir("hello")
+# os.mkdir("hello")
+
+"""shelve"""
+# import shelve
+#
+#
+# FILENAME = "states2"
+# with shelve.open(FILENAME) as states:
+#     states["London"] = "Great Britain"
+#     states["Paris"] = "France"
+#     states["Berlin"] = "Germany"
+#     states["Madrid"] = "Spain"
+#
+# with shelve.open(FILENAME) as states:
+#     print(states["London"])
+#     print(states["Madrid"])
+
+"""pickle"""
+# import pickle
+#
+#
+# FILENAME = "users.dat"
+#
+# users = [
+#     ["Tom", 28, True],
+#     ["Alice", 23, False],
+#     ["Bob", 34, False],
+# ]
+#
+# with open(FILENAME, "wb") as file:
+#     pickle.dump(users, file)
+#
+# with open(FILENAME, "rb") as file:
+#     users_from_file = pickle.load(file)
+#     for user in users_from_file:
+#         print("Name:", user[0], "\tAge:", user[1], "\tMarried:", user[2])
+
+# FILENAME = "user.dat"
+#
+# name = "Tom"
+# age = 19
+#
+# with open(FILENAME, "wb") as file:
+#     pickle.dump(name, file)
+#     pickle.dump(age, file)
+#
+# with open(FILENAME, "rb") as file:
+#     name = pickle.load(file)
+#     age = pickle.load(file)
+#     print("Name: ", name, "\tAge: ", age)
+
 """csv"""
-import csv
-
-
-FILENAME = "users.csv"
-
-users = [
-    ["Tom", 28],
-    ["Alice", 23],
-    ["Bob", 34],
-]
-
-with open(FILENAME, "w", newline="") as file:
-    writer = csv.writer(file)
-    writer.writerows(users)
-
-with open(FILENAME, "a", newline="") as file:
-    user = ["Sam", 31]
-    writer = csv.writer(file)
-    writer.writerow(user)
+# import csv
+#
+# FILENAME = "users.csv"
+#
+# users = [
+#     ["Tom", 28],
+#     ["Alice", 23],
+#     ["Bob", 34],
+# ]
+#
+# with open(FILENAME, "w", newline="") as file:
+#     writer = csv.writer(file)
+#     writer.writerows(users)
+#
+# with open(FILENAME, "a", newline="") as file:
+#     user = ["Sam", 31]
+#     writer = csv.writer(file)
+#     writer.writerow(user)
 
 """work with file"""
 # FILENAME = "messages.txt"
