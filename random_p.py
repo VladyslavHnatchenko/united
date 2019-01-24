34a5600afa850a83e42a564d41ea263a9f0b867e
vladyslav.hnatchenko <hnatchenko.vladyslav@gmail.com>
2019-01-24 07:53:32 +0200

    1/24/19

---
+++ random_p.py
@@ -0,0 +1,83 @@
+"""date"""
+
+
+"""decimal"""
+# from decimal import Decimal
+#
+#
+# number = Decimal("0.1")
+# number = number + number + number
+# print(number)
+
+"""locale"""
+# import locale
+#
+#
+# locale.setlocale(locale.LC_ALL, "")
+#
+# number = 12345.6789
+# formatted = locale.format_string("%.02f", number)
+# print(formatted)
+# print(locale.getlocale())
+
+"""math"""
+# import math
+#
+#
+# radius = 30
+# area = math.pi * math.pow(radius, 2)
+# print(area)
+#
+# number = math.log(10, math.e)
+# print(number)
+
+# n1 = math.pow(2, 3)
+# print(n1)
+#
+# n2 = 2**3
+# print(n2)
+#
+# print(math.sqrt(9))
+#
+# print(math.ceil(4.56))
+#
+# print(math.floor(4.56))
+#
+# print(math.radians(180))
+#
+# print(math.cos(math.radians(60)))
+#
+# print(math.sin(math.radians(90)))
+#
+# print(math.tan(math.radians(0)))
+#
+# print(math.log(8,2))
+#
+# print(math.log10(100))
+
+"""random"""
+import random
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+# random.shuffle(numbers)
+# print(numbers)
+#
+# random_number = random.choice(numbers)
+# print(random_number)
+
+# number = random.randrange(10)
+# print(number)
+#
+# number1 = random.randrange(2, 10)
+# print(number1)
+
+# number2 = random.randrange(2, 10, 2)
+# print(number2)
+
+# number = random.randint(20, 35)
+# print(number)
+
+# number = random.random()
+# print(number)
+#
+# number1 = random.random() * 100
+# print(number1)
