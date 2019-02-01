c47cb0356ad928c5888ca83cbe39a1559767301f
Vladyslav Hnatchenko <hnatchenko.vladyslav@gmail.com>
2019-02-01 08:05:28 +0200

    2/1/19

---
+++ prctc.py
@@ -0,0 +1,11 @@
+"""if-elif-else"""
+
+# old = int(input("Your age: "))
+# print("Recommendation:", end=' ')
+#
+# if 3 <= old < 6:
+#     print('"hare in the maze"')
+
+"""exceptions"""
+# 1a = 10
+# print(1a)
