8263a3ecc408c9244b4f0258c91f82f8a1743223
vladyslav.hnatchenko <hnatchenko.vladyslav@gmail.com>
2019-02-15 15:39:20 +0200

    2/15/19

--- pep8_practice.py
+++ pep8_practice.py
@@ -1,3 +1,7 @@
 print("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita esse,"
       " architecto voluptate quos suscipit ea nobis iure vel atque omnis, pariatur"
       " aut quasi nulla nostrum veniam itaque beatae sunt, adipisci.")
+vwerv
+ewvgfcewv
+
+
