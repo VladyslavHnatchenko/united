4d715a4fa6fee3e2d4d5cf8f0e10043929857554
Vladyslav Hnatchenko <hnatchenko.vladyslav@gmail.com>
2019-06-21 10:27:53 +0300

    finish asyncio module

---
+++ async/6_del.py
@@ -0,0 +1,32 @@
+def coroutine(func):
+    def inner(*args, **kwargs):
+        g = func(*args, **kwargs)
+        g.send(None)
+        return g
+    return inner
+
+
+class BlaBlaException(Exception):
+    pass
+
+
+# @coroutine
+def subgen():
+    while True:
+        try:
+            message = yield
+        except BlaBlaException:
+            print("Ku-ku!!!")
+        else:
+            print("..........", message)
+
+
+@coroutine
+def delegator(g):
+    # while True:
+    #     try:
+    #         data = yield
+    #         g.send(data)
+    #     except BlaBlaException as e:
+    #         g.throw(e)
+    yield from g
